735de8f2b41109e7b579244a0a32d5fd6caf18ef
mahmudaZaman <mahmudazaman0509@gmail.com>
2018-06-11 16:47:59 +0600

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import Users from './Users/Users';
+import Users from './Components/Users/Users';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 class App extends Component {  
   render() {
@@ -8,6 +12,13 @@ class App extends Component {
       <div>
         <Users title="User List"></Users>
         <h4>Routing</h4>
+        <Router>
+          <div>
+            <Route path="/" component={Home} exact />
+            <Route path="/about"  component={About} />
+            <Route path="/contact"  component={Contact} />
+          </div>
+        </Router>
       </div>
     )
   }
